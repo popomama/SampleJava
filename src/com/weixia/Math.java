bc84234a3a38a89bc6b7204e962edb72e9991b23
Wei Xia <wei_xia@hotmail.com>
2018-04-10 17:12:12 -0400

    first commit

---
+++ src/com/weixia/Math.java
@@ -0,0 +1,16 @@
+package com.weixia;
+
+import com.weixia.service.MessageService;
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MessageService ms = new MessageService();
+		
+		System.out.println(ms.getMessage());
+		
+	}
+
+}
